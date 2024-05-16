0e6c2f55d22fd056b59a5dd5b031e1e208fdd725
Ezekielle Louise Marie L. Gambong <104586559+EzekielleGambong@users.noreply.github.com>
2024-05-16 17:29:36 +0800

    last

--- server/utils/sendVerificationMail.js
+++ server/utils/sendVerificationMail.js
@@ -7,7 +7,7 @@ const sendVerificationMail = (user) => {
         from: '"PETPALS" <tartori_dev@outlook.com>',
         to: user.email,
         subject: "Verify your email...",
-        html: `<p>Good day adopter ${user.name}, verify your email by clicking this link... 
+        html: `<p>Good day adopter ${user.firstname}, verify your email by clicking this link... 
         <a href="http://127.0.0.1:5173/verify-email?emailToken=${user.emailToken}">Verify Your Email</a></p>`,
     };
     
